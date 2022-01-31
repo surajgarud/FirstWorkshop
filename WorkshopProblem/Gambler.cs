b3c4efb8884f2ba0c4c6ceaa12c1deb8a5292c70
Suraj Garud <surajgarud95@gmail.com>
2022-01-31 19:45:15 +0530

    GamblerPlayFirstBet

---
+++ WorkshopProblem/Gambler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkshopProblem
+{
+    internal class Gambler
+    {
+      public static void GamblerBet()
+      {
+            int stakePrDay = 100;
+            int betPrGame = 1;
+      }
+        public static void Startbet()
+        {
+            int GamblerWin = 0;
+            int Gamblerloos = 0;
+            Random random = new Random();
+            int check = random.Next(0, 2);
+            if (check == GamblerWin)
+                Console.WriteLine("Gambler Is Won");
+            else
+                Console.WriteLine("Gambler Is Lost");
+        }
+
+    }
+}
